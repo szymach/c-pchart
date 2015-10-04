0f71e75995ff70ae79ac523a2dfd944f85e4025a
Piotr Szymaszek <pszymaszek@gmail.com>
2015-10-04 12:08:56 +0200

    Updating gitignore and formating

--- src/Classes/pBarcode39.php
+++ src/Classes/pBarcode39.php
@@ -1,19 +1,20 @@
 <?php
+
 namespace CpChart\Classes;
 
-/*
-    pBarcode39 - class to create barcodes (39B)
-
-    Version     : 2.1.4
-    Made by     : Jean-Damien POGOLOTTI
-    Last Update : 19/01/2014
-
-    This file can be distributed under the license you can find at :
-
-                      http://www.pchart.net/license
-
-    You can find the whole class documentation on the pChart web site.
-*/
+/**
+ *  pBarcode39 - class to create barcodes (39B)
+ *
+ *  Version     : 2.1.4
+ *  Made by     : Jean-Damien POGOLOTTI
+ *  Last Update : 19/01/2014
+ *
+ *  This file can be distributed under the license you can find at :
+ *
+ *  http://www.pchart.net/license
+ *
+ *  You can find the whole class documentation on the pChart web site.
+ */
 class pBarcode39
 {
     public $Codes;
@@ -29,22 +30,22 @@ class pBarcode39
      * @param type $EnableMOD43
      * @throws \Exception
      */
-    function __construct($BasePath = "", $EnableMOD43 = false)
+    public function __construct($BasePath = "", $EnableMOD43 = false)
     {
-        $this->MOD43   = $EnableMOD43;
-        $this->Codes   = "";
+        $this->MOD43 = $EnableMOD43;
+        $this->Codes = "";
         $this->Reverse = "";
-        if (file_exists($BasePath."data/39.db", "r")) {
-            $FileHandle = @fopen($BasePath."data/39.db", "r");
-            $filePath = $BasePath."data/39.db";
+        if (file_exists($BasePath . "data/39.db", "r")) {
+            $FileHandle = @fopen($BasePath . "data/39.db", "r");
+            $filePath = $BasePath . "data/39.db";
         } else {
-            $FileHandle = @fopen(__DIR__.'/../Resources/data/39.db', "r");
-            $filePath = "/../Resources/data/39.db";            
+            $FileHandle = @fopen(__DIR__ . '/../Resources/data/39.db', "r");
+            $filePath = "/../Resources/data/39.db";
         }
 
-        if (!$FileHandle) { 
+        if (!$FileHandle) {
             throw new \Exception(
-                "Cannot find barcode database (".$filePath.")."
+            "Cannot find barcode database (" . $filePath . ")."
             );
         }
 
@@ -65,30 +66,30 @@ class pBarcode39
      * @param type $Format
      * @return type
      */
-    function getSize($TextString, $Format = "")
+    public function getSize($TextString, $Format = "")
     {
-        $Angle		= isset($Format["Angle"]) ? $Format["Angle"] : 0;
-        $ShowLegend	= isset($Format["ShowLegend"]) ? $Format["ShowLegend"] : false;
-        $LegendOffset	= isset($Format["LegendOffset"]) ? $Format["LegendOffset"] : 5;
-        $DrawArea	= isset($Format["DrawArea"]) ? $Format["DrawArea"] : false;
-        $FontSize	= isset($Format["FontSize"]) ? $Format["FontSize"] : 12;
-        $Height		= isset($Format["Height"]) ? $Format["Height"] : 30;
+        $Angle = isset($Format["Angle"]) ? $Format["Angle"] : 0;
+        $ShowLegend = isset($Format["ShowLegend"]) ? $Format["ShowLegend"] : false;
+        $LegendOffset = isset($Format["LegendOffset"]) ? $Format["LegendOffset"] : 5;
+        $DrawArea = isset($Format["DrawArea"]) ? $Format["DrawArea"] : false;
+        $FontSize = isset($Format["FontSize"]) ? $Format["FontSize"] : 12;
+        $Height = isset($Format["Height"]) ? $Format["Height"] : 30;
 
-        $TextString    = $this->encode39($TextString);
+        $TextString = $this->encode39($TextString);
         $BarcodeLength = strlen($this->Result);
-        
-        $WOffset = $DrawArea ? 20 : 0 ;
+
+        $WOffset = $DrawArea ? 20 : 0;
         $HOffset = $ShowLegend ? $FontSize + $LegendOffset + $WOffset : 0;
 
         $X1 = cos($Angle * PI / 180) * ($WOffset + $BarcodeLength);
         $Y1 = sin($Angle * PI / 180) * ($WOffset + $BarcodeLength);
 
-        $X2 = $X1 + cos(($Angle+90) * PI / 180) * ($HOffset+$Height);
-        $Y2 = $Y1 + sin(($Angle+90) * PI / 180) * ($HOffset+$Height);
+        $X2 = $X1 + cos(($Angle + 90) * PI / 180) * ($HOffset + $Height);
+        $Y2 = $Y1 + sin(($Angle + 90) * PI / 180) * ($HOffset + $Height);
 
 
-        $AreaWidth  = max(abs($X1),abs($X2));
-        $AreaHeight = max(abs($Y1),abs($Y2));
+        $AreaWidth = max(abs($X1), abs($X2));
+        $AreaHeight = max(abs($Y1), abs($Y2));
 
         return(array("Width" => $AreaWidth, "Height" => $AreaHeight));
     }
@@ -98,29 +99,29 @@ class pBarcode39
      * @param type $Value
      * @return string
      */
-    function encode39($Value)
+    public function encode39($Value)
     {
-        $this->Result = "100101101101"."0";
-        $TextString   = "";
-        for ($i=1; $i <= strlen($Value); $i++) {
-            $CharCode = ord($this->mid($Value,$i,1));
-            if ($CharCode >= 97 && $CharCode <= 122) { 
-                $CharCode = $CharCode - 32;                 
+        $this->Result = "100101101101" . "0";
+        $TextString = "";
+        for ($i = 1; $i <= strlen($Value); $i++) {
+            $CharCode = ord($this->mid($Value, $i, 1));
+            if ($CharCode >= 97 && $CharCode <= 122) {
+                $CharCode = $CharCode - 32;
             }
 
             if (isset($this->Codes[chr($CharCode)])) {
-                $this->Result = $this->Result.$this->Codes[chr($CharCode)]."0";
-                $TextString = $TextString.chr($CharCode);
+                $this->Result = $this->Result . $this->Codes[chr($CharCode)] . "0";
+                $TextString = $TextString . chr($CharCode);
             }
         }
 
         if ($this->MOD43) {
             $Checksum = $this->checksum($TextString);
-            $this->Result = $this->Result.$this->Codes[$Checksum]."0";
+            $this->Result = $this->Result . $this->Codes[$Checksum] . "0";
         }
 
-        $this->Result = $this->Result."100101101101";
-        $TextString   = "*".$TextString."*";
+        $this->Result = $this->Result . "100101101101";
+        $TextString = "*" . $TextString . "*";
 
         return $TextString;
     }
@@ -133,66 +134,64 @@ class pBarcode39
      * @param type $Y
      * @param type $Format
      */
-    function draw($Object,$Value,$X,$Y,$Format="")
+    public function draw($Object, $Value, $X, $Y, $Format = "")
     {
         $this->pChartObject = $Object;
 
-        $R		= isset($Format["R"]) ? $Format["R"] : 0;
-        $G		= isset($Format["G"]) ? $Format["G"] : 0;
-        $B		= isset($Format["B"]) ? $Format["B"] : 0;
-        $Alpha		= isset($Format["Alpha"]) ? $Format["Alpha"] : 100;
-        $Height		= isset($Format["Height"]) ? $Format["Height"] : 30;
-        $Angle		= isset($Format["Angle"]) ? $Format["Angle"] : 0;
-        $ShowLegend	= isset($Format["ShowLegend"]) ? $Format["ShowLegend"] : false;
-        $LegendOffset	= isset($Format["LegendOffset"]) ? $Format["LegendOffset"] : 5;
-        $DrawArea	= isset($Format["DrawArea"]) ? $Format["DrawArea"] : false;
-        $AreaR		= isset($Format["AreaR"]) ? $Format["AreaR"] : 255;
-        $AreaG		= isset($Format["AreaG"]) ? $Format["AreaG"] : 255;
-        $AreaB		= isset($Format["AreaB"]) ? $Format["AreaB"] : 255;
-        $AreaBorderR	= isset($Format["AreaBorderR"]) ? $Format["AreaBorderR"] : $AreaR;
-        $AreaBorderG	= isset($Format["AreaBorderG"]) ? $Format["AreaBorderG"] : $AreaG;
-        $AreaBorderB	= isset($Format["AreaBorderB"]) ? $Format["AreaBorderB"] : $AreaB;
+        $R = isset($Format["R"]) ? $Format["R"] : 0;
+        $G = isset($Format["G"]) ? $Format["G"] : 0;
+        $B = isset($Format["B"]) ? $Format["B"] : 0;
+        $Alpha = isset($Format["Alpha"]) ? $Format["Alpha"] : 100;
+        $Height = isset($Format["Height"]) ? $Format["Height"] : 30;
+        $Angle = isset($Format["Angle"]) ? $Format["Angle"] : 0;
+        $ShowLegend = isset($Format["ShowLegend"]) ? $Format["ShowLegend"] : false;
+        $LegendOffset = isset($Format["LegendOffset"]) ? $Format["LegendOffset"] : 5;
+        $DrawArea = isset($Format["DrawArea"]) ? $Format["DrawArea"] : false;
+        $AreaR = isset($Format["AreaR"]) ? $Format["AreaR"] : 255;
+        $AreaG = isset($Format["AreaG"]) ? $Format["AreaG"] : 255;
+        $AreaB = isset($Format["AreaB"]) ? $Format["AreaB"] : 255;
+        $AreaBorderR = isset($Format["AreaBorderR"]) ? $Format["AreaBorderR"] : $AreaR;
+        $AreaBorderG = isset($Format["AreaBorderG"]) ? $Format["AreaBorderG"] : $AreaG;
+        $AreaBorderB = isset($Format["AreaBorderB"]) ? $Format["AreaBorderB"] : $AreaB;
 
-        $TextString   = $this->encode39($Value);
+        $TextString = $this->encode39($Value);
 
         if ($DrawArea) {
-            $X1 = $X + cos(($Angle-135) * PI / 180) * 10;
-            $Y1 = $Y + sin(($Angle-135) * PI / 180) * 10;
+            $X1 = $X + cos(($Angle - 135) * PI / 180) * 10;
+            $Y1 = $Y + sin(($Angle - 135) * PI / 180) * 10;
 
-            $X2 = $X1 + cos($Angle * PI / 180) * (strlen($this->Result)+20);
-            $Y2 = $Y1 + sin($Angle * PI / 180) * (strlen($this->Result)+20);
+            $X2 = $X1 + cos($Angle * PI / 180) * (strlen($this->Result) + 20);
+            $Y2 = $Y1 + sin($Angle * PI / 180) * (strlen($this->Result) + 20);
 
             if ($ShowLegend) {
-                $X3 = $X2 + cos(($Angle+90) * PI / 180) 
-                    * ($Height+$LegendOffset+$this->pChartObject->FontSize+10);
-                $Y3 = $Y2 + sin(($Angle+90) * PI / 180) 
-                    * ($Height+$LegendOffset+$this->pChartObject->FontSize+10);
+                $X3 = $X2 + cos(($Angle + 90) * PI / 180) * ($Height + $LegendOffset + $this->pChartObject->FontSize + 10);
+                $Y3 = $Y2 + sin(($Angle + 90) * PI / 180) * ($Height + $LegendOffset + $this->pChartObject->FontSize + 10);
             } else {
-                $X3 = $X2 + cos(($Angle+90) * PI / 180) * ($Height+20);
-                $Y3 = $Y2 + sin(($Angle+90) * PI / 180) * ($Height+20);
+                $X3 = $X2 + cos(($Angle + 90) * PI / 180) * ($Height + 20);
+                $Y3 = $Y2 + sin(($Angle + 90) * PI / 180) * ($Height + 20);
             }
 
-            $X4 = $X3 + cos(($Angle+180) * PI / 180) * (strlen($this->Result)+20);
-            $Y4 = $Y3 + sin(($Angle+180) * PI / 180) * (strlen($this->Result)+20);
+            $X4 = $X3 + cos(($Angle + 180) * PI / 180) * (strlen($this->Result) + 20);
+            $Y4 = $Y3 + sin(($Angle + 180) * PI / 180) * (strlen($this->Result) + 20);
 
-            $Polygon  = array($X1, $Y1, $X2, $Y2, $X3, $Y3, $X4, $Y4);
+            $Polygon = array($X1, $Y1, $X2, $Y2, $X3, $Y3, $X4, $Y4);
             $Settings = array(
                 "R" => $AreaR,
                 "G" => $AreaG,
-                "B" =>$AreaB,
+                "B" => $AreaB,
                 "BorderR" => $AreaBorderR,
                 "BorderG" => $AreaBorderG,
                 "BorderB" => $AreaBorderB
             );
-            $this->pChartObject->drawPolygon($Polygon,$Settings);
+            $this->pChartObject->drawPolygon($Polygon, $Settings);
         }
 
-        for ($i=1; $i <= strlen($this->Result); $i++) {
-            if ($this->mid($this->Result,$i,1) == 1) {
+        for ($i = 1; $i <= strlen($this->Result); $i++) {
+            if ($this->mid($this->Result, $i, 1) == 1) {
                 $X1 = $X + cos($Angle * PI / 180) * $i;
                 $Y1 = $Y + sin($Angle * PI / 180) * $i;
-                $X2 = $X1 + cos(($Angle+90) * PI / 180) * $Height;
-                $Y2 = $Y1 + sin(($Angle+90) * PI / 180) * $Height;
+                $X2 = $X1 + cos(($Angle + 90) * PI / 180) * $Height;
+                $Y2 = $Y1 + sin(($Angle + 90) * PI / 180) * $Height;
 
                 $Settings = array("R" => $R, "G" => $G, "B" => $B, "Alpha" => $Alpha);
                 $this->pChartObject->drawLine($X1, $Y1, $X2, $Y2, $Settings);
@@ -200,11 +199,11 @@ class pBarcode39
         }
 
         if ($ShowLegend) {
-            $X1 = $X + cos($Angle * PI / 180) * (strlen($this->Result)/2);
-            $Y1 = $Y + sin($Angle * PI / 180) * (strlen($this->Result)/2);
+            $X1 = $X + cos($Angle * PI / 180) * (strlen($this->Result) / 2);
+            $Y1 = $Y + sin($Angle * PI / 180) * (strlen($this->Result) / 2);
 
-            $LegendX = $X1 + cos(($Angle+90) * PI / 180) * ($Height+$LegendOffset);
-            $LegendY = $Y1 + sin(($Angle+90) * PI / 180) * ($Height+$LegendOffset);
+            $LegendX = $X1 + cos(($Angle + 90) * PI / 180) * ($Height + $LegendOffset);
+            $LegendY = $Y1 + sin(($Angle + 90) * PI / 180) * ($Height + $LegendOffset);
 
             $Settings = array(
                 "R" => $R,
@@ -218,30 +217,31 @@ class pBarcode39
         }
     }
 
-    function checksum($string)
+    public function checksum($string)
     {
         $checksum = 0;
-        $length   = strlen($string);
-        $charset  = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ-. $/+%';
+        $length = strlen($string);
+        $charset = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ-. $/+%';
 
-        for( $i=0; $i < $length; ++$i ) {
-            $checksum += strpos( $charset, $string[$i] );
+        for ($i = 0; $i < $length; ++$i) {
+            $checksum += strpos($charset, $string[$i]);
         }
-        return substr( $charset, ($checksum % 43), 1 );
+        return substr($charset, ($checksum % 43), 1);
     }
 
-    function left($value, $NbChar)
-    { 
-        return substr($value, 0, $NbChar);         
-    }  
-    
-    function right($value, $NbChar) 
-    { 
+    public function left($value, $NbChar)
+    {
+        return substr($value, 0, $NbChar);
+    }
+
+    public function right($value, $NbChar)
+    {
         return substr($value, strlen($value) - $NbChar, $NbChar);
-    }  
-    
-    function mid($value, $Depart, $NbChar)
-    { 
-        return substr($value, $Depart-1, $NbChar);         
-    }  
+    }
+
+    public function mid($value, $Depart, $NbChar)
+    {
+        return substr($value, $Depart - 1, $NbChar);
+    }
+
 }
--- src/Services/pChartFactory.php
+++ src/Services/pChartFactory.php
@@ -3,9 +3,10 @@ namespace CpChart\Services;
 
 use CpChart\Classes\pData;
 use CpChart\Classes\pImage;
+use Exception;
 
 /**
- * A simple service class utilizing the Factory design pattern. It has three 
+ * A simple service class utilizing the Factory design pattern. It has three
  * class specific methods, as well as a generic loader for the chart classes.
  *
  * @author szymach @ http://github.com/szymach
@@ -13,17 +14,17 @@ use CpChart\Classes\pImage;
 class pChartFactory
 {
     private $namespace = 'CpChart\Classes\\';
-    
+
     /**
      * Loads a new chart class (scatter, pie etc.). Some classes require instances of
-     * pImage and pData classes passed into their constructor. These classes are: 
-     * pBubble, pPie, pScatter, pStock, pSurface and pIndicator. Otherwise the 
+     * pImage and pData classes passed into their constructor. These classes are:
+     * pBubble, pPie, pScatter, pStock, pSurface and pIndicator. Otherwise the
      * pChartObject and pDataObject parameters are redundant.
-     * 
+     *
      * ATTENTION! SOME OF THE CHARTS NEED TO BE DRAWN VIA A METHOD FROM THE
-     * pIMAGE CLASS (ex. 'drawBarChart'), NOT THROUGH THIS METHOD! READ THE 
+     * pIMAGE CLASS (ex. 'drawBarChart'), NOT THROUGH THIS METHOD! READ THE
      * DOCUMENTATION FOR MORE DETAILS.
-     * 
+     *
      * @param string $chartType - type of the chart to be loaded (for example 'pie', not 'pPie')
      * @param pImage $pChartObject
      * @param pData $pDataObject
@@ -31,24 +32,24 @@ class pChartFactory
      */
     public function newChart(
         $chartType,
-        pImage $pChartObject = null, 
+        pImage $pChartObject = null,
         pData $pDataObject = null
     ) {
         $this->checkChartType($chartType);
         $className = $this->namespace.'p'.ucfirst($chartType);
         if (!class_exists($className)) {
-            throw new \Exception('The requested chart class does not exist!');
+            throw new Exception('The requested chart class does not exist!');
         }
         return new $className($pChartObject, $pDataObject);
     }
-    
+
     /**
      * Checks if the requested chart type is created via one of the methods in
      * the pDraw class, instead through a seperate class. If a method in pDraw
      * exists, an exception with proper information is thrown.
-     * 
+     *
      * @param string $chartType
-     * @throws \Exception
+     * @throws Exception
      */
     private function checkChartType($chartType)
     {
@@ -58,7 +59,7 @@ class pChartFactory
         );
         foreach ($methods as $method) {
             if (method_exists($this->namespace.'pImage', $method)) {
-                throw new \Exception(
+                throw new Exception(
                     'The requested chart is not a seperate class, to draw it you'
                   . ' need to call the "'.$method.'" method on the pImage object'
                   . ' after populating it with data!'
@@ -67,10 +68,10 @@ class pChartFactory
             }
         }
     }
-    
+
     /**
      * Creates a new pData class with an option to pass the data to form a serie.
-     * 
+     *
      * @param array $points - points to be added to serie
      * @param string $serieName - name of the serie
      * @return pData
@@ -78,17 +79,17 @@ class pChartFactory
     public function newData(array $points = array(), $serieName = "Serie1")
     {
         $className = $this->namespace.'pData';
-        $data = new $className(); 
+        $data = new $className();
         if (count($points) > 0) {
             $data->addPoints($points, $serieName);
         }
         return $data;
     }
-    
+
     /**
      * Create a new pImage class. It requires the size of axes to be properly
      * constructed.
-     * 
+     *
      * @param integer $XSize - length of the X axis
      * @param integer $YSize - length of the Y axis
      * @param pData $DataSet - pData class populated with points
@@ -98,10 +99,10 @@ class pChartFactory
     public function newImage(
         $XSize,
         $YSize,
-        \CpChart\Classes\pData $DataSet = null,
+        pData $DataSet = null,
         $TransparentBackground = false
     ) {
-        $className = $this->namespace.'pImage';
+        $className = sprintf('%spImage', $this->namespace);
         return new $className(
             $XSize,
             $YSize,
@@ -109,22 +110,22 @@ class pChartFactory
             $TransparentBackground
         );
     }
-    
+
     /**
      * Create one of the pBarcode classes. Only the number is required (39 or 128),
      * the class name is contructed on the fly. Passing the constructor's parameters
      * is also available, but not mandatory.
-     * 
+     *
      * @param string $number - number identifing the pBarcode class ("39" or "128")
      * @param string $BasePath - optional path for the file containing the class data
      * @param boolean $EnableMOD43
      * @return pBarcode(39|128)
-     * @throws \Exception
+     * @throws Exception
      */
     public function getBarcode($number, $BasePath = "", $EnableMOD43 = false)
     {
         if ($number != "39" && $number != "128") {
-            throw new \Exception(
+            throw new Exception(
                 'The barcode class for the provided number does not exist!'
             );
         }
