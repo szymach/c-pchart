50d1c2dd4adbd3f5f29de325c3395c5baee50476
Piotr Szymaszek <pszymaszek@gmail.com>
2017-02-25 16:52:35 +0100

    Changed ChartIsAMethodException message

--- src/Exception/ChartIsAMethodException.php
+++ src/Exception/ChartIsAMethodException.php
@@ -9,12 +9,6 @@ use Exception;
  */
 class ChartIsAMethodException extends FactoryException
 {
-    protected $message = 'The requested chart is not a seperate class, to draw'
-        . ' it you need to call the "%s" method on the Image object'
-        . ' after populating it with data!'
-        . ' Check the documentation on library\'s website for details.'
-    ;
-
     /**
      * @param string $method - the method which is supposed to be used
      * @param int $code
@@ -22,6 +16,11 @@ class ChartIsAMethodException extends FactoryException
      */
     public function __construct($method, $code = null, Exception $previous = null)
     {
-        parent::__construct(sprintf($this->message, $method), $code, $previous);
+        $message = 'The requested chart is not a seperate class, to draw'
+            . ' it you need to call the "%s" method on the Image object'
+            . ' after populating it with data!'
+            . ' Check the documentation on library\'s website for details.'
+        ;
+        parent::__construct(sprintf($message, $method), $code, $previous);
     }
 }
