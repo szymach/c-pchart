5e51794878be474695aa79edf5103540c89cef1e
Yannick Warnier <ywarnier@beeznest.org>
2017-03-06 16:31:42 -0500

    Add fixes for PHP 7.1 support - refs #1

--- src/Chart/Draw.php
+++ src/Chart/Draw.php
@@ -1134,7 +1134,7 @@ abstract class Draw extends BaseDraw
             );
         }
 
-        $this->Mask = "";
+        $this->Mask = [];
         $Color = $this->allocateColor($this->Picture, $R, $G, $B, $Alpha);
         for ($i = 0; $i <= $Radius * 2; $i++) {
             $Slice = sqrt($Radius * $Radius - ($Radius - $i) * ($Radius - $i));
@@ -2457,7 +2457,7 @@ abstract class Draw extends BaseDraw
                     $Y = $Y + max($this->FontSize * count($Lines), $IconAreaHeight) + 5;
                 } elseif ($Mode == LEGEND_HORIZONTAL) {
                     $Lines = preg_split("/\n/", $Serie["Description"]);
-                    $Width = "";
+                    $Width = [];
                     foreach ($Lines as $Key => $Value) {
                         $BoxArray = $this->drawText(
                             $X + $IconAreaWidth + 4,
